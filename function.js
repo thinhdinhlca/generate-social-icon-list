770cb34059ee89d29e10b3d9fccf9958634601c3
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-02-09 06:51:43 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,13 +1,14 @@
-window.function = function (linkedin, twitter, facebook, website, instagram, email) {
+window.function = function (linkedin, twitter, facebook, website, instagram, youtube, email) {
 
   linkedin = linkedin.value ?? "";
   twitter = twitter.value ?? "";
   facebook = facebook.value ?? "";
   website = website.value ?? "";
   instagram = instagram.value ?? "";
+  youtube = youtube.value ?? "";
   email = email.value ?? "";
 
-  if (linkedin == "" && twitter == "" && facebook == "" && website == "" & instagram == "" & email == "") {
+  if (linkedin == "" && twitter == "" && facebook == "" && website == "" && instagram == "" && youtube == "" && email == "") {
     return "";
   }
 
@@ -18,6 +19,7 @@ window.function = function (linkedin, twitter, facebook, website, instagram, ema
   let fb_HTML = "";
   let wb_HTML = "";
   let in_HTML = "";
+  let yt_HTML = "";
   let em_HTML = "";
 
   let li_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/YPtpsMvhgMpWz75UGhmO.png";
@@ -25,6 +27,7 @@ window.function = function (linkedin, twitter, facebook, website, instagram, ema
   let fb_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/wLTNVGtLqXIpxp6071pC.png";
   let wb_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/AwbRCNrb6hjAR0t21DaF.png";
   let in_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/4Tv9Xcfhk9RG45LagySl.png";
+  let yt_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/8Ztn4F5yvPrRtmkF9sz2.png";
   let em_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/n0zQvPjrhWqtr7GwTt5s.png";
   
 if (linkedin != "") {
@@ -46,12 +49,17 @@ if (linkedin != "") {
   if (instagram != "") {
     in_HTML = `<div class = "social-column"> \n \n <a href="${instagram}" target="blank"><img src="${in_img}" width= "30" height = "30" ></a></div>`;
   }
+    
+  if (youtube != "") {
+    yt_HTML = `<div class = "social-column"> \n \n <a href="${youtube}" target="blank"><img src="${yt_img}" width= "30" height = "30" ></a></div>`;
+  }
+
 
   if (email != "") {
     em_HTML = `<div class = "social-column"> \n \n <a href="mailto:${email}" target="blank"><img src="${em_img}" width= "30" height = "30" ></a></div>`;
   }
   
-  let social_joined = [li_HTML, tw_HTML, fb_HTML, wb_HTML, in_HTML, em_HTML].filter(Boolean).join("\n \n ");
+  let social_joined = [li_HTML, tw_HTML, fb_HTML, wb_HTML, in_HTML, yt_HTML, em_HTML].filter(Boolean).join("\n \n ");
 
   let final = `<div class="social-row">
   \n \n ${social_joined}
