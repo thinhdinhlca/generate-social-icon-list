59cfe0a0847df202a0f1bdd2fccc0fc8c9836330
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2023-12-15 11:41:50 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 window.function = function (linkedin, twitter, facebook, website, instagram, youtube, email, whatsapp, pinterest, tiktok, telegram, yelp, phone, message) {
 
+  // Extract values or set default to empty string
   linkedin = linkedin.value ?? "";
   twitter = twitter.value ?? "";
   facebook = facebook.value ?? "";
@@ -15,121 +16,56 @@ window.function = function (linkedin, twitter, facebook, website, instagram, you
   phone = phone.value ?? "";
   message = message.value ?? "";
 
+  // Check if all values are empty
   if (phone == "" && message == "" && yelp == "" && telegram == "" && linkedin == "" && twitter == "" && facebook == "" && website == "" && instagram == "" && youtube == "" && email == "" && whatsapp == "" && pinterest == "" && tiktok == "") {
     return "";
   }
 
-  else {
-  let tl_HTML = "";
-  let li_HTML = "";
-  let tw_HTML = "";
-  let fb_HTML = "";
-  let wb_HTML = "";
-  let in_HTML = "";
-  let yt_HTML = "";
-  let em_HTML = "";
-  let ws_HTML = "";
-  let pi_HTML = "";
-  let tt_HTML = "";
-  let ye_HTML = "";
-  let ph_HTML = "";
-  let ms_HTML = "";
-    
-    //Image links
-  let tl_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/UBQEtHCGAaUCK6SmkKKI.png";
-  let li_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/YPtpsMvhgMpWz75UGhmO.png";
-  let tw_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/mAWXwE6VcIS5Qf7eiV88.png";
-  let fb_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/wLTNVGtLqXIpxp6071pC.png";
-  let wb_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/AwbRCNrb6hjAR0t21DaF.png";
-  let in_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/4Tv9Xcfhk9RG45LagySl.png";
-  let yt_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/cLvEnvaG13inhm1E5PFX.png";
-  let em_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/pbOlDrCcoqJUQSStog3U.png";
-  let ws_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/ACcKtnFmkOOV7lsTHcV9/pub/tKpquLCJ9SpOEPWLuNV6.png";
-  let pi_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/aERzn549fzlrRK6IuaKk.png";
-  let tt_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/kADuVCdD7LsazzPaOiQv.png";
-  let ye_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/FEz2yDNF2weU0PhBn3wh.png";
-  let ph_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/mEfOSTRFMtF01CxDkREg.png";
-  let ms_img = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/r21OIORoDpKuA16rTxqR.png";
-  
-  if (phone != "") {
-    ph_HTML = `<div class = "social-column"> \n \n <a href="tel:${phone}" target="blank"><img src="${ph_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (message != "") {
-    ms_HTML = `<div class = "social-column"> \n \n <a href="sms://${message}" target="blank"><img src="${ms_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (yelp != "") {
-    ye_HTML = `<div class = "social-column"> \n \n <a href="${yelp}" target="blank"><img src="${ye_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (telegram != "") {
-    tl_HTML = `<div class = "social-column"> \n \n <a href="${telegram}" target="blank"><img src="${tl_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (linkedin != "") {
-    li_HTML = `<div class = "social-column"> \n \n <a href="${linkedin}" target="blank"><img src="${li_img}" width= "30" height = "30" ></a></div>`;
+  // Define base HTML structure with inline styles
+  const createSocialLink = (type, value, imgSrc) => {
+    let url;
+    if (type === "email") {
+      url = `mailto:${value}`;
+    } else if (type === "phone") {
+      url = `tel:${value}`;
+    } else {
+      url = value;
+    }
+    return `<div style="position: relative; display: inline-block; float: right; padding: 10px;"> 
+              <a href="${url}" target="blank">
+                <img src="${imgSrc}" style="width: 30px; height: 30px;">
+              </a>
+            </div>`;
+  };
+
+    // Image links for each social media
+  const imgSources = {
+    linkedin: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/YPtpsMvhgMpWz75UGhmO.png",
+    twitter: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/mAWXwE6VcIS5Qf7eiV88.png",
+    facebook: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/wLTNVGtLqXIpxp6071pC.png",
+    website: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/Mr755ISwcRYn8zBd2L3z/pub/AwbRCNrb6hjAR0t21DaF.png",
+    instagram: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/4Tv9Xcfhk9RG45LagySl.png",
+    youtube: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/cLvEnvaG13inhm1E5PFX.png",
+    email: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/pbOlDrCcoqJUQSStog3U.png",
+    whatsapp: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/ACcKtnFmkOOV7lsTHcV9/pub/tKpquLCJ9SpOEPWLuNV6.png",
+    pinterest: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/aERzn549fzlrRK6IuaKk.png",
+    tiktok: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/kADuVCdD7LsazzPaOiQv.png",
+    telegram: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/UBQEtHCGAaUCK6SmkKKI.png",
+    yelp: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/FEz2yDNF2weU0PhBn3wh.png",
+    phone: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/mEfOSTRFMtF01CxDkREg.png",
+    message: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/kGe07Qg5m2NzxUKiptUf/pub/r21OIORoDpKuA16rTxqR.png"
+  };
+
+  // Generate HTML for each social media link
+  let socialHTML = "";
+  for (const [key, value] of Object.entries({ linkedin, twitter, facebook, website, instagram, youtube, email, whatsapp, pinterest, tiktok, telegram, yelp, phone, message })) {
+    if (value) {
+      socialHTML += createSocialLink(value, imgSources[key]);
+    }
   }
 
-  if (twitter != "") {
-    tw_HTML = `<div class = "social-column"> \n \n <a href="${twitter}" target="blank"><img src="${tw_img}" width= "30" height = "30" ></a></div>`;
-  }
-
-  if (facebook != "") {
-    fb_HTML = `<div class = "social-column"> \n \n <a href="${facebook}" target="blank"><img src="${fb_img}" width= "30" height = "30" ></a></div>`;
-  }
-
-   if (website != "") {
-    wb_HTML = `<div class = "social-column"> \n \n <a href="${website}" target="blank"><img src="${wb_img}" width= "30" height = "30" ></a></div>`;
-  } 
-
-  if (instagram != "") {
-    in_HTML = `<div class = "social-column"> \n \n <a href="${instagram}" target="blank"><img src="${in_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (youtube != "") {
-    yt_HTML = `<div class = "social-column"> \n \n <a href="${youtube}" target="blank"><img src="${yt_img}" width= "30" height = "30" ></a></div>`;
-  }
-
-  if (email != "") {
-    em_HTML = `<div class = "social-column"> \n \n <a href="mailto:${email}" target="blank"><img src="${em_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (whatsapp != "") {
-    ws_HTML = `<div class = "social-column"> \n \n <a href="${whatsapp}" target="blank"><img src="${ws_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (pinterest != "") {
-    pi_HTML = `<div class = "social-column"> \n \n <a href="${pinterest}" target="blank"><img src="${pi_img}" width= "30" height = "30" ></a></div>`;
-  }
-    
-  if (tiktok != "") {
-    tt_HTML = `<div class = "social-column"> \n \n <a href="${tiktok}" target="blank"><img src="${tt_img}" width= "30" height = "30" ></a></div>`;
-  }
-  
-  let social_joined = [li_HTML, tw_HTML, fb_HTML, wb_HTML, in_HTML, yt_HTML, em_HTML, ws_HTML,pi_HTML,tt_HTML,tl_HTML,ye_HTML,ph_HTML,ms_HTML].filter(Boolean).join("\n \n ");
-
-  let final = `<div class="social-row">
-  \n \n ${social_joined}
-  \n \n <pre><span><style>
-  \n \n .social-column {
-  \n   position: relative;
-  \n display: inline-block;
-  \n float: right;
-  \n padding: 10px;
-\n }
-
-\n \n .social-column img {
-  \n width: 30px !important;
-  \n height: 30px !important;
-\n }
-
-\n \n .social-row {
-\n display: flex;
-\n align-items: center;
-\n justify-content: center;
-\n }`;
-
-  return final;
-}
-}
+  // Final HTML structure
+  return `<div style="display: flex; align-items: center; justify-content: center;">
+            ${socialHTML}
+          </div>`;
+};
